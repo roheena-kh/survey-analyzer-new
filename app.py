ef77389bfc45d5fd54ef73b63699d4699cc97c88
roheena-kh <roheenakhairi1@gmail.com>
2025-05-20 10:09:09 +0430

    survey analyzer renamed to servey_analyzer

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from survey_analyzer import run_survey_analysis
-
+import pandas as pd
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with a strong secret key
 
